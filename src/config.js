4e4336b95c59cc94e26a3e87696e668b200aa7c4
yopaseopor <yopaseopor@gmail.com>
2022-08-02 12:37:32 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -846,7 +846,7 @@ style: function (feature) {
 			iconSrc: imgSrc + 'icones/fire_hydrant.svg',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
 			style: function (feature) {
-				var name = feature.get('fire_hydrant:diameter') || '';
+				var name = feature.get('fire_hydrant:diameter') || ''
 				var name2 = feature.get('survey:date') || '';
 				var styles = {
 					'fire_hydrant:diameter': {

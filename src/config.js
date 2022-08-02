f40d986b81be65ae8e4e2ff0d75b8f089438a045
yopaseopor <yopaseopor@gmail.com>
2022-08-02 02:28:49 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -807,7 +807,7 @@ style: function (feature) {
 							text: new ol.style.Text({
 								text: name,
 								offsetX : 7,
-								offsetY : -8,
+								offsetY : -12,
 								fill: new ol.style.Fill({
                             color: 'rgba(0,0,0,1)'
                         }),

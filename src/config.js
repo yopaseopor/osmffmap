51c9d9c1495eda94984dc153c6aec69acdd38877
yopaseopor <yopaseopor@gmail.com>
2022-10-14 21:11:51 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -874,7 +874,7 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'icones/ses_logo.png',
-							scale:0.07
+							scale:0.03
 						}),
 							text: new ol.style.Text({
 								text: name,

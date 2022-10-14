889ae5c9f2ca64eb1aa2eb4128f3806c199eb4fb
yopaseopor <yopaseopor@gmail.com>
2022-10-14 21:21:59 +0200

    Health

--- src/config.js
+++ src/config.js
@@ -889,6 +889,78 @@ style: function (feature) {
 				});
 				return style;
 			}
+  },
+		{
+			group: 'Emergency',
+			title: 'Hospital',
+			query: '(nwr["amenity"="hospital"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/hospital_logo.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(117,63,79,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(117,63,79,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/hospital_logo.png',
+							scale:0.02
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -10,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
+  },
+		{
+			group: 'Emergency',
+			title: 'Clinic',
+			query: '(nwr["amenity"="clinic"]({{bbox}});node(w););out meta;',
+			iconSrc: imgSrc + 'icones/cap_logo.png',
+			iconStyle: 'background-color:rgba(255,255,255,0.4)',
+style: function (feature) {
+				var key_regex = /^name$/
+				var name_key = feature.getKeys().filter(function(t){return t.match(key_regex)}).pop() || "name"
+				var name = feature.get(name_key) || '';
+				var fill = new ol.style.Fill({
+					color: 'rgba(117,63,79,0.4)'
+				});
+				var stroke = new ol.style.Stroke({
+					color: 'rgba(117,63,79,1)',
+					width: 1
+				});
+				var style = new ol.style.Style({
+					image: new ol.style.Icon({
+							src: imgSrc + 'icones/cap_logo.png',
+							scale:0.02
+						}),
+							text: new ol.style.Text({
+								text: name,
+								offsetX : 7,
+								offsetY : -10,
+								fill: new ol.style.Fill({
+                            color: 'rgba(0,0,0,1)'
+                        }),
+						}),
+					fill: fill,
+					stroke: stroke
+				});
+				return style;
+			}
 		},
 		{
 			group: 'Emergency',

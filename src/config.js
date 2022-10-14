d6c6fce7ac0a99cace58c82c71a6d7f2f61b796c
yopaseopor <yopaseopor@gmail.com>
2022-10-14 21:05:21 +0200

    Ses

--- src/config.js
+++ src/config.js
@@ -802,7 +802,7 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'icones/police_logo.png',
-							scale:0.07
+							scale:0.02
 						}),
 							text: new ol.style.Text({
 								text: name,
@@ -838,7 +838,7 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'icones/firestation_logo.png',
-							scale:0.07
+							scale:0.02
 						}),
 							text: new ol.style.Text({
 								text: name,
@@ -858,7 +858,7 @@ style: function (feature) {
 			group: 'Emergency',
 			title: 'Protección Civil (SES)',
 			query: '(nwr["emergency"="ses_station"]({{bbox}});node(w););out meta;',
-			iconSrc: imgSrc + 'icones/fire_hydrant.svg',
+			iconSrc: imgSrc + 'icones/ses_logo.png',
 			iconStyle: 'background-color:rgba(255,255,255,0.4)',
 style: function (feature) {
 				var key_regex = /^name$/
@@ -873,7 +873,7 @@ style: function (feature) {
 				});
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
-							src: imgSrc + 'icones/hydrant_sign.svg',
+							src: imgSrc + 'icones/ses_logo.png',
 							scale:0.07
 						}),
 							text: new ol.style.Text({

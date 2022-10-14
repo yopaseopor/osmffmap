be6755120ee635d30eb16f4b3e26f110c97a3ce3
yopaseopor <yopaseopor@gmail.com>
2022-10-14 21:35:38 +0200

    Update config.js

--- src/config.js
+++ src/config.js
@@ -946,7 +946,7 @@ style: function (feature) {
 				var style = new ol.style.Style({
 					image: new ol.style.Icon({
 							src: imgSrc + 'icones/clinic_logo.svg',
-							scale:0.02
+							scale:0.07
 						}),
 							text: new ol.style.Text({
 								text: name,
